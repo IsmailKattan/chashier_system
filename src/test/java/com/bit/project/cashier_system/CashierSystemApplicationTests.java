a4e00a253d84eca71fd85c7289e87a119cdd8cc2
ismail kattan <ismail.kattan@ogr.sakarya.edu.tr>
2024-04-30 14:49:30 +0300

    initial commit

---
+++ src/test/java/com/bit/project/cashier_system/CashierSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.project.cashier_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashierSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
