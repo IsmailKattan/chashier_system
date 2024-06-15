649d4313107bff54f775771557f3f61b1777bed6
İsmail Kattan <103933327+IsmailKattan@users.noreply.github.com>
2024-06-15 19:10:12 +0300

    DTO's have been created

--- src/main/java/com/_32bit/project/cashier_system/DTO/SaleItemDTO.java
+++ src/main/java/com/_32bit/project/cashier_system/DTO/SaleItemDTO.java
@@ -1,21 +1,26 @@
 package com._32bit.project.cashier_system.DTO;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-
 @Getter
 @AllArgsConstructor
 @RequiredArgsConstructor
-public class RoleDTO {
+public class SaleItemDTO {
 
     private final long id;
     @Setter
-    private String name;
+    private double quantity;
+    @Setter
+    private double total;
+    @Setter
+    private double discountRate;
+    @Setter
+    private double discountedPrice;
     @Setter
     private boolean deleted;
-
+    private final SaleDTO sale;
+    private final ProductDTO product;
 }
